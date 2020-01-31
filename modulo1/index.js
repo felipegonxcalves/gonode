8465fca64b1cd5416c8d3195ba1559ed161446fe
felipeconxceicao <felipe.conceicao@infocast.com.br>
2020-01-31 17:51:42 -0300

    Entendendo o fluxo do express

--- modulo1/index.js
+++ modulo1/index.js
@@ -1,6 +1,31 @@
-const http = require('http');
+const express = require('express');
 
-http.createServer((req, res) => {
-    console.log(req); // req é usado para pegar as informações da requisição
-    return res.end('Hello World'); // res é o objeto usado para devolver uma resposta ao cliente
-}).listen(3000);
+const app = express();
+
+const logMiddleware = (req, res, next) => {
+    console.log(
+        `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method} `
+    );
+
+    // CASO QUEIRA ADICIONAR UM VALOR DENTRO DO REP PRA SER CHAMADO EM ALGUMA ROTA
+    req.appName = 'GoNode';
+
+    return next(); // Colocando esse next a gente está falando ao Middleware que a gente não deve interceptar o fluxo da requisição
+};
+
+// POSSO FAZER TODAS AS ROTAS DA APLICAÇÃO USAREM UM DETERMINADO MIDDLEWARE
+// app.use(logMiddleware);
+
+app.get('/', logMiddleware, (req, res) => {
+    // veja que pego o valor appName passado na Middleware
+    return res.send(`Bem-vindo ao ${req.appName}, ${req.query.name}`);
+});
+
+app.get("/nome/:name", (req, res) => {
+    // return res.send(`Bem-vindo, ${req.params.name}`);
+    return res.json({
+        message: `Bem-vindo, ${req.params.name}`
+    });
+});
+
+app.listen(3000);
