4442a129509db8a4f8b6ff5b05b853f602884eae
felipeconxceicao <felipe.conceicao@infocast.com.br>
2020-02-11 18:32:56 -0300

    Atualizando master

--- modulo1/index.js
+++ modulo1/index.js
@@ -1,31 +1,26 @@
 const express = require('express');
+const nunjucks = require('nunjucks'); 
+// Nunjucks é uma templating engine, uma forma de nós renderizarmos e manipularmos HTML
+// com conteúdo Javascript facilmente;
 
 const app = express();
 
-const logMiddleware = (req, res, next) => {
-    console.log(
-        `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method} `
-    );
-
-    // CASO QUEIRA ADICIONAR UM VALOR DENTRO DO REP PRA SER CHAMADO EM ALGUMA ROTA
-    req.appName = 'GoNode';
-
-    return next(); // Colocando esse next a gente está falando ao Middleware que a gente não deve interceptar o fluxo da requisição
-};
-
-// POSSO FAZER TODAS AS ROTAS DA APLICAÇÃO USAREM UM DETERMINADO MIDDLEWARE
-// app.use(logMiddleware);
-
-app.get('/', logMiddleware, (req, res) => {
-    // veja que pego o valor appName passado na Middleware
-    return res.send(`Bem-vindo ao ${req.appName}, ${req.query.name}`);
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app, // Aqui fica a variável que está no servidor Express,
+    watch: true,
 });
 
-app.get("/nome/:name", (req, res) => {
-    // return res.send(`Bem-vindo, ${req.params.name}`);
-    return res.json({
-        message: `Bem-vindo, ${req.params.name}`
-    });
+app.set("view engine", "njk");
+
+const users = ['Felipe Gonçalves', 'Paulo Roberto', 'Andre Walace'];
+
+app.get('/', (req, res) => {
+    return res.render('list', {users});
+});
+
+app.get('/new', (req, res) => {
+    return res.render("new");
 });
 
 app.listen(3000);
