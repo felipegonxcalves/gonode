611bcc463ed9142dc67c184c77d507f1e571a66e
Felipe Gonçalves <salvandofelipe@hotmail.com>
2020-02-12 06:35:59 -0300

    Finalizando inicio de estudo básico do Node.js

--- modulo1/index.js
+++ modulo1/index.js
@@ -10,7 +10,8 @@ nunjucks.configure('views', {
     express: app, // Aqui fica a variável que está no servidor Express,
     watch: true,
 });
-
+// Isso diz para o express saber lidar com as informações que são provenientes de um formulário HTML
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "njk");
 
 const users = ['Felipe Gonçalves', 'Paulo Roberto', 'Andre Walace'];
@@ -21,6 +22,11 @@ app.get('/', (req, res) => {
 
 app.get('/new', (req, res) => {
     return res.render("new");
+});
+
+app.post('/create', (req, res) => {
+    users.push(req.body.user);
+    return res.redirect('/');
 });
 
 app.listen(3000);
